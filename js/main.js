971125a6c607740a56ab7b1b69bc0a450bd39734
Juan Escobar <juan.ingles.8619@gmail.com>
2024-05-07 07:42:24 -0500

    branch: commands. deleted login, logout functions

--- js/main.js
+++ js/main.js
@@ -1,40 +1,57 @@
 const tasks = [];
 
-function logIn() { };
-
-function logOut() { };
-
-function editProfile() { };
-
 function addNewTask(id, name, details, priority, dueDate) {
-      const task = {
-            id: id,
-            name: name,
-            details: details,
-            priority: priority,
-            dueDate: dueDate
-      }
-      tasks.push(task);
-};
+  const task = {
+    id: id,
+    name: name,
+    details: details,
+    priority: priority,
+    dueDate: dueDate,
+  };
+  tasks.push(task);
+}
 
 function asignPriority(priority) {
-      tasks.push(task.priority)
-};
+  tasks.push(task.priority);
+}
 
-function asignDueDate() { };
+function asignDueDate() {}
 
-function markTaskAsCompleted() { };
+function markTaskAsCompleted() {}
 
-function deleteTasks() { };
+function deleteTasks() {}
 
 function displayTasks() {
-      for (const task of tasks) {
-            alert("ID: " + task.id + "; Name: " + task.name + "; details: " + task.details + "; priority: " + task.priority + "; Due Date: " + task.dueDate)
-      }
-};
+  for (const task of tasks) {
+    alert(
+      "ID: " +
+        task.id +
+        "; Name: " +
+        task.name +
+        "; details: " +
+        task.details +
+        "; priority: " +
+        task.priority +
+        "; Due Date: " +
+        task.dueDate
+    );
+  }
+}
 
-addNewTask("001", "Finish the project", "Testing Details 1. Details added.", "Low", "03/05/2024");
-addNewTask("002", "Create schedule", "Testing Details 2. More details added", "High", "04/05/2024");
+addNewTask(
+  "001",
+  "Finish the project",
+  "Testing Details 1. Details added.",
+  "Low",
+  "03/05/2024"
+);
+addNewTask(
+  "002",
+  "Create schedule",
+  "Testing Details 2. More details added",
+  "High",
+  "04/05/2024"
+);
 
 displayTasks();
 console.log(tasks);
